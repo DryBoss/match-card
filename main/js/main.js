970893b17cf06425226e40d2b3002be660536460
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2023-03-21 17:01:53 +0600

    js progress

--- main/js/main.js
+++ main/js/main.js
@@ -1,7 +1,24 @@
 const timeShow = document.querySelector("#time span");
 const card1 = document.querySelector("#c1");
+const card2 = document.querySelector("#c2");
+const card3 = document.querySelector("#c3");
+const card4 = document.querySelector("#c4");
+const card5 = document.querySelector("#c5");
+const card6 = document.querySelector("#c6");
+const card7 = document.querySelector("#c7");
+const card8 = document.querySelector("#c8");
+const card9 = document.querySelector("#c9");
+const card10 = document.querySelector("#c10");
+const card11 = document.querySelector("#c11");
+const card12 = document.querySelector("#c12");
+const card13 = document.querySelector("#c13");
+const card14 = document.querySelector("#c14");
+const card15 = document.querySelector("#c15");
+const card16 = document.querySelector("#c16");
 
 let cardStatus = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+let selectedCard = "";
 
 (function timeCountdown () {
   let time = 120;
@@ -12,13 +29,22 @@ let cardStatus = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 })();
 
 function cardClicked (cardNumber, cardId) {
-  if (cardStatus[cardId] === 0) {
-    cardNumber.classList.add("targeted");
+  if (selectedCard === "") {
+    cardNumber.classList.add("selected");
+    cardStatus[cardId] = 1;
+    selectedCard = cardNumber;
+  } else if (selectedCard === cardNumber) {
+    cardNumber.classList.remove("selected");
+    cardStatus[cardId] = 0;
+    selectedCard = "";
+  } else if (selectedCard !== cardNumber) {
+    cardNumber.classList.add("card-flip")
+    setTimeout(() => {
+      cardNumber.classList.add("revealed")
+    }, 250)
+    setTimeout(() => {
+      cardNumber.classList.remove("card-flip")
+      cardNumber.classList.remove("revealed")
+    }, 500)
   }
-  cardNumber.style.transform = "scale(0, 1)";
-  const cardAnimation = setInterval(() => {
-    cardNumber.style.transform = "scale(1, 1)";
-    clearInterval(cardAnimation);
-  }, 250)
 }
-
