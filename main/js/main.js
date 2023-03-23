45f1c7e08fca6957d26f19410c922f65d6944749
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2023-03-24 02:07:29 +0600

    js progress

--- main/js/main.js
+++ main/js/main.js
@@ -1,6 +1,8 @@
 const timeShow = document.querySelector("#time span");
 const card1 = document.querySelector("#c1");
+const card1Img = document.querySelector("#c1-img");
 const card2 = document.querySelector("#c2");
+const card2Img = document.querySelector("#c2-img");
 const card3 = document.querySelector("#c3");
 const card4 = document.querySelector("#c4");
 const card5 = document.querySelector("#c5");
@@ -16,9 +18,9 @@ const card14 = document.querySelector("#c14");
 const card15 = document.querySelector("#c15");
 const card16 = document.querySelector("#c16");
 
-let cardStatus = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-
 let selectedCard = "";
+let selectedCardImg = "";
+let cards = [];
 
 (function timeCountdown () {
   let time = 120;
@@ -28,17 +30,21 @@ let selectedCard = "";
   }, 1000)
 })();
 
-function cardClicked (cardNumber, cardId) {
+function cardClicked (cardNumber, cardNumberImg) {
   if (selectedCard === "") {
     cardNumber.classList.add("selected");
     selectedCard = cardNumber;
+    selectedCardImg = cardNumberImg;
   } else if (selectedCard === cardNumber) {
     cardNumber.classList.remove("selected");
     selectedCard = "";
+    selectedCardImg = "";
   } else if (selectedCard !== cardNumber) {
     cardNumber.classList.add("selected")
     cardNumber.classList.add("card-flip")
+    cardNumberImg.src = "images/cards/4-of-hearts.png"
     selectedCard.classList.add("card-flip")
+    selectedCardImg.src = "images/cards/4-of-hearts.png"
     setTimeout(() => {
       cardNumber.classList.add("revealed")
       selectedCard.classList.add("revealed")
@@ -54,3 +60,13 @@ function cardClicked (cardNumber, cardId) {
     }, 1000)
   }
 }
+
+function genarateCards() {
+  for(let looped = 0; looped < 16; looped++) {
+    let pickedCard;
+    do {pickedCard = Math.floor(Math.random() * 8)} while (cards.filter(card => card === pickedCard).length > 1);
+    cards.push(pickedCard);
+  }
+}
+
+genarateCards();
